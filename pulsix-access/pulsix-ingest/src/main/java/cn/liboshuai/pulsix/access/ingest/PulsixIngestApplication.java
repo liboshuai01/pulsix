07a78dc8b0cb481835ba32578b63061a7197ec63
Boshuai Li <liboshuai01@gmail.com>
2026-03-17 22:52:00 +0800

    fix(ingest): 编译报错

---
+++ pulsix-access/pulsix-ingest/src/main/java/cn/liboshuai/pulsix/access/ingest/PulsixIngestApplication.java
@@ -0,0 +1,16 @@
+package cn.liboshuai.pulsix.access.ingest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * pulsix-ingest 启动类。
+ */
+@SpringBootApplication
+public class PulsixIngestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PulsixIngestApplication.class, args);
+    }
+
+}
